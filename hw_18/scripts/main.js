1035ff31984328d3f87282f87ebbe4c317d11863
filyafilkin <johnnywolf159@gmail.com>
2022-05-25 23:06:50 +0300

    add 4 exercise

--- hw_18/scripts/main.js
+++ hw_18/scripts/main.js
@@ -13,3 +13,10 @@ let flashVolume = prompt ('How many gigabytes in your flash drive?');
 let flashAmmount = flashVolume / 0.82;
 let flashInteger = Math.round(flashAmmount);
 alert (flashInteger);
+
+// 4 exercise
+let questCash = prompt ('How much money do you have?');
+let questPrice = prompt ('What is the price of chocolate?');
+let questAmmount = questCash / questPrice;
+let numberChoc =  Math.round(questAmmount);
+alert (`You will get ${questAmmount} chocolates!`);
