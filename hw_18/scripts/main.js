9e3a9188192510145b4c04d4510b7dcd7f65e9d3
filyafilkin <johnnywolf159@gmail.com>
2022-05-25 22:59:38 +0300

    add hw_18

---
+++ hw_18/scripts/main.js
@@ -0,0 +1,15 @@
+// 1 exercise
+let ammount1 = 0.1 + +0.2;
+alert (ammount1.toFixed(1));
+
+// 2 exercise
+let num1 = 1;
+n = 2;
+let ammount2 = num1 + 2;
+alert (ammount2);
+
+// 3 exercise
+let flashVolume = prompt ('How many gigabytes in your flash drive?');
+let flashAmmount = flashVolume / 0.82;
+let flashInteger = Math.round(flashAmmount);
+alert (flashInteger);
