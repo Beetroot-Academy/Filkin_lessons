695d06a42b56d4f20389684719c813e23a235895
filyafilkin <johnnywolf159@gmail.com>
2022-05-29 11:41:33 +0300

    bag fix

--- hw_18/scripts/main.js
+++ hw_18/scripts/main.js
@@ -1,28 +1,30 @@
 // 1 exercise
-let ammount1 = 0.1 + +0.2;
+let ammount1 = 0.1 + 0.2;
 alert (ammount1.toFixed(1));
 
 // 2 exercise
 let num1 = '1';
 let num2 = 2;
-let ammount2 = +num1 + +num2;
-alert (ammount2);
+let ammount2 = +num1 + num2;
+alert(ammount2);
 
 // 3 exercise
 let flashVolume = prompt ('How many gigabytes in your flash drive?');
 let flashAmmount = flashVolume / 0.82;
 let flashInteger = Math.round(flashAmmount);
-alert (flashInteger);
+alert(flashInteger);
 
 // 4 exercise
 let questCash = prompt ('How much money do you have?');
 let questPrice = prompt ('What is the price of chocolate?');
 let questAmmount = questCash / questPrice;
-let numberChoc =  Math.round(questAmmount);
-alert (`You will get ${questAmmount} chocolates!`);
+let numberChoc =  Math.floor(questAmmount);
+let changeChoc = questCash - questPrice * numberChoc;
+alert(`You will get ${numberChoc} chocolates and your change is ${changeChoc} dollars!`);
 
 // 5 exercise 
 let questNum = prompt ('Say a three digit number!');
+
 function getReversedNum(questNum) {
     let questResult = 0;
     while (questNum) {
@@ -30,14 +32,15 @@ function getReversedNum(questNum) {
       questNum = Math.floor(questNum / 10);
     }
     return questResult;
+
   }
-  alert (`Your number - ${getReversedNum(questNum)}!`);
+  alert(`Your number - ${getReversedNum(questNum)}!`);
 
 //   6 exercise
 let questMoney = prompt ('Tell me the amount of your contribution!');
 let moneyPrecent = questMoney * 0.05;
-let moneyCash = (+moneyPrecent + +questMoney) * 0.05;
-alert (`You will get ${moneyCash}!`);
+let moneyCash = (moneyPrecent + +questMoney) * 0.05;
+alert(`You will get ${moneyCash}!`);
 
 // 7 exercise
 let test = 2 && 0 && 3; // 0
