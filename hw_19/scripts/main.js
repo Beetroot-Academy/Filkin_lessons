0cc434c55c95205abb93f37f00bebd45383af87e
filyafilkin <johnnywolf159@gmail.com>
2022-06-11 12:58:40 +0300

    .

---
+++ hw_19/scripts/main.js
@@ -0,0 +1,72 @@
+// // 1 exercise 
+// let userAge = +prompt ('How old are you?');
+// switch (true) {
+//     case (userAge >= 0 && userAge <= 11) :
+//         alert ('You are children!')
+//         break;
+//     case (userAge >= 12 && userAge <= 17) :
+//         alert ('You are teenager!')
+//         break;
+//     case (userAge >= 18 && userAge <= 59) :
+//         alert ('You are adult!')
+//         break;
+//     case (userAge >= 60) :
+//         alert ('You are pensioner!')
+//         break;
+//     default:
+//         alert ('Error!');
+// }
+
+// // 2 exercise 
+// let userNum = +prompt ('Enter a number from 0 to 9!');
+// switch (userNum) {
+//     case 0:
+//       alert(')');
+//       break;
+//     case 1:
+//       alert('!');
+//       break;
+//     case 2:
+//       alert('@');
+//       break;
+//     case 3:
+//       alert('#');
+//       break;
+//     case 4:
+//       alert('$');
+//       break;
+//     case 5:
+//       alert('%');
+//       break;
+//     case 6:
+//       alert('^');
+//       break;
+//     case 7:
+//       alert('&');
+//       break;
+//     case 8:
+//       alert('*');
+//       break;
+//     case 9:
+//       alert('(');
+//       break;
+//     default:
+//       alert('Error!');
+//   }
+
+// //   3 exercise 
+
+// let num1 = prompt ('Enter the first number in the range');
+// let num2 = prompt ('Enter the second number in the range');
+// let sum = 0;
+// while(num1<num2){
+//   num1++;
+//   sum+=num1;    
+// }
+
+// alert(`Sum of your range is ${sum}!`);
+
+// // 4 exercise 
+
+// let numDevider1 = prompt ('Name the first number');
+// let numDevider2 = prompt ('Name the second number');
