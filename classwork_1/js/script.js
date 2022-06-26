80f16c0c58bafefdba9dd9e5e7b96f466a8df525
Andrey_Sakhnenko <senya310594@gmail.com>
2022-06-26 13:47:21 +0300

    add post

--- classwork_1/js/script.js
+++ classwork_1/js/script.js
@@ -2,6 +2,27 @@ let form = document.querySelector('form')
 let inputEmail = document.querySelector("#email")
 let inputPass = document.querySelector("#pass")
 
-form.addEventListener('submit', function submit(){
-    console.log(1)
+
+form.addEventListener('submit',  async function submit(event){
+event.preventDefault()
+    let post = {
+        "email": inputEmail.value,
+        "password": inputPass.value,
+      }
+     let respons = await fetch('https://awesome-wallet-app.herokuapp.com/auth/login', {
+          method: "POST",
+          body: JSON.stringify(post),
+          headers: {
+            "Content-Type": "application/json;charset=utf-8",
+    }
+        })
+        let res = await respons.json();
+        console.log(res)
+        if(res.code == 200){
+            let tokin = res.data.accessToken;
+            document.cookie = `Authorization=${tokin}`;
+            console.log('registration successfull');
+        }else{
+            console.log('error');
+        }
 })
