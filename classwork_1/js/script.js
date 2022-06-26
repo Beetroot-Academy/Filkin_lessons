5cda60c09de3a3b8ca5449854d256e781cb2d372
filyafilkin <johnnywolf159@gmail.com>
2022-06-26 13:15:04 +0300

    add

---
+++ classwork_1/js/script.js
@@ -0,0 +1,7 @@
+let form = document.querySelector('form')
+let inputEmail = document.querySelector("#email")
+let inputPass = document.querySelector("#pass")
+
+form.addEventListener('submit', function submit(){
+    console.log(1)
+})
