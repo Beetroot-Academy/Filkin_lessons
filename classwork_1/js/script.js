a81ad68005571dbac1ebd96ec0ae05a7e6701de8
Vladimir Kholiavskiy <kholiavskiy@gmail.com>
2022-06-26 14:05:01 +0300

    added task 2 part 1

--- classwork_1/js/script.js
+++ classwork_1/js/script.js
@@ -1,30 +1,40 @@
-let form = document.querySelector("form");
-let inputEmail = document.querySelector("#email");
-let inputPass = document.querySelector("#pass");
+let form = document.querySelector('form');
+let inputEmail = document.querySelector('#email');
+let inputPass = document.querySelector('#pass');
 
-form.addEventListener("submit", async function submit(event) {
-  event.preventDefault();
-  let post = {
-    email: inputEmail.value,
-    password: inputPass.value,
-  };
-  let respons = await fetch(
-    "https://awesome-wallet-app.herokuapp.com/auth/login",
-    {
-      method: "POST",
-      body: JSON.stringify(post),
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
-    }
-  );
-  let res = await respons.json();
-  console.log(res);
-  if (res.code == 200) {
-    let tokin = res.data.accessToken;
-    document.cookie = `Authorization=${tokin}`;
-    console.log("registration successfull");
-  } else {
-    console.log("error");
-  }
+form.addEventListener('submit', async function submit(event) {
+	event.preventDefault();
+	let post = {
+		email: inputEmail.value,
+		password: inputPass.value,
+	};
+	let respons = await fetch(
+		'https://awesome-wallet-app.herokuapp.com/auth/login',
+		{
+			method: 'POST',
+			body: JSON.stringify(post),
+			headers: {
+				'Content-Type': 'application/json;charset=utf-8',
+			},
+		}
+	);
+	let res = await respons.json();
+
+	if (res.code == 200) {
+		window.location.replace(
+			'http://127.0.0.1:5501/classwork_1/registration.html'
+		);
+		let tokin = res.data.accessToken;
+		document.cookie = `Authorization=${tokin}`;
+
+		console.log('registration successfull');
+	} else {
+		alert('error');
+	}
+});
+
+let textarea = document.querySelector('#textarea');
+
+textarea.addEventListener('change', (e) => {
+	localStorage.setItem('text', textarea.value);
 });
