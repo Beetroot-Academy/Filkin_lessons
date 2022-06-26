8501f4c24d7256ddba74c6294ae5b7969e05ff28
Andrey_Sakhnenko <senya310594@gmail.com>
2022-06-26 14:38:31 +0300

    add dark

--- classwork_1/js/script.js
+++ classwork_1/js/script.js
@@ -1,58 +1,61 @@
-let form = document.querySelector('form');
-let inputEmail = document.querySelector('#email');
-let inputPass = document.querySelector('#pass');
+let form = document.querySelector("form");
+let inputEmail = document.querySelector("#email");
+let inputPass = document.querySelector("#pass");
 
-form.addEventListener('submit', async function submit(event) {
-	event.preventDefault();
-	let post = {
-		email: inputEmail.value,
-		password: inputPass.value,
-	};
-	let respons = await fetch(
-		'https://awesome-wallet-app.herokuapp.com/auth/login',
-		{
-			method: 'POST',
-			body: JSON.stringify(post),
-			headers: {
-				'Content-Type': 'application/json;charset=utf-8',
-			},
-		}
-	);
-	let res = await respons.json();
-
-	if (res.code == 200) {
-		window.location.replace(
-			'http://127.0.0.1:5501/classwork_1/registration.html'
-		);
-		let tokin = res.data.accessToken;
-		document.cookie = `Authorization=${tokin}`;
-
-		console.log('registration successfull');
-	} else {
-		alert('error');
-	}
-});
-
-let textarea = document.querySelector('#textarea');
-
-textarea.addEventListener('change', (e) => {
-	localStorage.setItem('text', textarea.value);
-});
-
-document.addEventListener('DOMContentLoaded', (e) => {
-    if (localStorage.getItem('text')) {
-        textarea.value = localStorage.getItem('text');
+form.addEventListener("submit", async function submit(event) {
+  event.preventDefault();
+  let post = {
+    email: inputEmail.value,
+    password: inputPass.value,
+  };
+  let respons = await fetch(
+    "https://awesome-wallet-app.herokuapp.com/auth/login",
+    {
+      method: "POST",
+      body: JSON.stringify(post),
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
     }
+  );
+  let res = await respons.json();
 
+  if (res.code == 200) {
+    window.location.replace(
+      "http://127.0.0.1:5501/classwork_1/registration.html"
+    );
+    let tokin = res.data.accessToken;
+    document.cookie = `Authorization=${tokin}`;
+
+    console.log("registration successfull");
+  } else {
+    alert("error");
+  }
 });
 
-let switcher = document.querySelector('#switch');
-let body = document.querySelector('body')
-switcher.addEventListener('click', () => {
-body.classList.toggle('dark')
+let textarea = document.querySelector("#textarea");
 
-if(body.classList.contains('dark')) {
-localStorage.setItem('dark', true);
-}else{
-	localStorage.setItem('dark', false);
-}})
+textarea.addEventListener("change", (e) => {
+  localStorage.setItem("text", textarea.value);
+});
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  if (localStorage.getItem("dark")) {
+    body.classList.add("dark");
+  }
+  if (localStorage.getItem("text")) {
+    textarea.value = localStorage.getItem("text");
+  }
+});
+
+let switcher = document.querySelector("#switch");
+let body = document.querySelector("body");
+switcher.addEventListener("click", () => {
+  body.classList.toggle("dark");
+
+  if (body.classList.contains("dark")) {
+    localStorage.setItem("dark", true);
+  } else {
+    localStorage.removeItem("dark");
+  }
+});
