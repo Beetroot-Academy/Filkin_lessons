3b5d05d1278378876c6e37ae52485c9809feae35
filyafilkin <johnnywolf159@gmail.com>
2022-07-01 17:48:07 +0300

    add

--- hw_25/scripts/script.js
+++ hw_25/scripts/script.js
@@ -1,63 +1,74 @@
-document.addEventListener( 'DOMContentLoaded', function() {
-  let heroSplide = new Splide ('#hero-splide', {
-  type: 'loop',
-  cover: true,
-	perPage: 1,
-	perMove: 1,
-	autoplay: true,
-	interval: 3000,
-	arrows: false,
-	rewind: true,
+document.addEventListener("DOMContentLoaded", function () {
+  let heroSplide = new Splide("#hero-splide", {
+    type: "loop",
+    cover: true,
+    perPage: 1,
+    perMove: 1,
+    autoplay: true,
+    interval: 3000,
+    arrows: false,
+    rewind: true,
+    width: "100vw",
+  });
+
+  let arivalsSplide = new Splide("#arivals-splide", {
+    type: "loop",
+    perPage: 5,
+    perMove: 1,
+    pagination: false,
+    autoplay: true,
+    interval: 5000,
+    rewind: true,
+    gap: "20px",
+    classes: {
+      pagination: "arivals-splide__pagination",
+      page: "splide__pagination__page arivals-splide__pagination__page",
+    },
+    breakpoints: {
+      767: {
+        perPage: 1,
+        arrows: false,
+        pagination: true,
+      },
+      1320: {
+        perPage: 3,
+        gap: "15px",
+      },
+    },
+  });
+
+  let partnersSplide = new Splide("#partners-splide", {
+    type: "loop",
+    perPage: 9,
+    perMove: 1,
+    autoplay: true,
+    interval: 3000,
+    gap: "20px",
+    pagination: false,
+    rewind: true,
+    breakpoints: {
+      767: {
+        perPage: 2,
+        gap: "20px",
+        arrows: false,
+        pagination: true,
+      },
+      1320: {
+        perPage: 6,
+        gap: "10px",
+      },
+    },
+  });
+
+  partnersSplide.mount();
+  arivalsSplide.mount();
+  heroSplide.mount();
 });
 
-let arivalsSplide = new Splide ('#arivals-splide', {
-  type: 'loop',
-	perPage: 5,
-	perMove: 1,
-	pagination: false,
-  autoplay: true,
-	interval: 5000,
-	rewind: true,
-  gap: '20px',
-  breakpoints: {
-		768: {
-			perPage: 1,
-			arrows: false,
-			pagination: true,
-		},
-		1320: {
-			perPage: 3,
-			gap: '15px',
-		},
-	},
+let burger = document.querySelector(".menu");
+let nav = document.querySelector(".header-nav");
+
+burger.addEventListener("click", function showMenu() {
+  burger.classList.toggle("menu--active");
+  nav.classList.toggle("nav--active");
 });
-
-let partnersSplide = new Splide ('#partners-splide', {
-	type: 'loop',
-	perPage: 9,
-	perMove: 1,
-	autoplay: true,
-	interval: 3000,
-	gap: '20px',
-	pagination: false,
-	rewind: true,
-	breakpoints: {
-		768: {
-			perPage: 2,
-			gap: '20px',
-			arrows: false,
-			pagination: true,
-		},
-		1320: {
-			perPage: 6,
-			gap: '10px',
-		},
-	},
-})
-
-partnersSplide.mount();
-arivalsSplide.mount();
-heroSplide.mount();
-});
-
-
