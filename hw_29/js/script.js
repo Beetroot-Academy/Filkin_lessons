3f8917d1495dc511678ae61e326708eec8c91ac3
filyafilkin <johnnywolf159@gmail.com>
2022-07-18 13:13:31 +0300

    need to add animation

--- hw_29/js/script.js
+++ hw_29/js/script.js
@@ -1,16 +1,16 @@
-setInterval(function flip() {
-  let toSelect = 1;
-  const blocks = document.getElementById("blocks").children;
-  const used = [];
+// setInterval(function flip() {
+//   let toSelect = 1;
+//   const blocks = document.getElementById("blocks").children;
+//   const used = [];
 
-  while (toSelect !== 0) {
-    const index = Math.floor(Math.random() * blocks.length);
+//   while (toSelect !== 0) {
+//     const index = Math.floor(Math.random() * blocks.length);
 
-    if (used.lenght === blocks.length) break;
-    if (used.includes(index)) continue;
+//     if (used.lenght === blocks.length) break;
+//     if (used.includes(index)) continue;
 
-    blocks[index].classList.toggle("flip-vertical-right");
-    used.push(index);
-    toSelect--;
-  }
-}, 2000);
+//     blocks[index].classList.toggle("flip-vertical-right");
+//     used.push(index);
+//     toSelect--;
+//   }
+// }, 2000);
